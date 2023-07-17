0a5e22b980b416e378a3056cf0391ba6c93cafc2
Rajeev <rkrajeevnov@gmail.com>
2023-07-17 17:04:19 +0530

    add all requied libraries and folder and files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'Rajeev Kumar',
+    author_email= 'rkrajeevnov@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
